a243dbe02969aca25a0f183d5f4495b2795853fc
Nick Roshdieh <nickroshd@gmail.com>
2022-04-17 22:54:11 -0400

    typescript checking works

--- backend/server.ts
+++ backend/server.ts
@@ -3,8 +3,10 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 8080
 
+const currency: number = 1000;
+
 app.get("/", (req, res) => {
-    res.send('Well Done Typescript, now will it run as a build?');
+    res.send(`Well Done Typescript, you have $${currency}`);
 })
 
 app.listen(port, () => {
