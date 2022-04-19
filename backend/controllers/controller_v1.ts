2a995880e028bfb6e6529b528bd6202260c2f003
Nick Roshdieh <nickroshd@gmail.com>
2022-04-19 00:17:22 -0400

    api for search now working, with query parameter also working

--- backend/controllers/controller_v1.ts
+++ backend/controllers/controller_v1.ts
@@ -2,35 +2,31 @@ import 'dotenv/config';
 const axios = require('axios').default;
 
 const yelpSearch = async (
+    sort_by: string = 'review_count',
     latitude: number = 38.985,
-    longitude: number = -77.095, 
-    sort_by: string = 'review_count'
+    longitude: number = -77.095 
     ) => {
     const API_KEY = process.env.API_KEY
     const URL = `https://api.yelp.com/v3/businesses/search?open_now=true&categories=restaurant&latitude=${latitude}&longitude=${longitude}&sort_by=${sort_by}`
-    const response = await axios({
+    return await axios({
         url: URL,
         method: 'get',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${API_KEY}`
         }
-    }).then(function (responseData: any) {
-        console.log(responseData.data)
-        return responseData?.data
     })
 }
-
 
 const health = (req: any, res: any) => {
     res.send("API is working")
 }
 
-
 const search = async (req: any, res: any) => {
-    const searchData = await yelpSearch()
-    console.log(searchData)
-    res.send(searchData)
+    let sort_by = req.query.sort_by;
+    const searchData = await yelpSearch(sort_by)
+    console.log(searchData?.data)
+    res.send(searchData?.data)
 }
 
 export { search, health }
